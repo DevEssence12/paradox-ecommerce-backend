6d51a622b1e4095bafc5fcb33ea51741a6246c54
shivamp9381 <shivamp9381@gmail.com>
2025-02-12 08:44:28 +0530

    email id updated

--- controller/User.js
+++ controller/User.js
@@ -1,6 +1,26 @@
 const { Category } = require('../model/Category');
 const { User } = require('../model/User');
 
+exports.createAdmin = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    // Use the static method from the User model to create an admin
+    const admin = await User.createAdmin(email, password, name);
+    
+    res.status(201).json({
+      id: admin.id,
+      email: admin.email,
+      role: admin.role
+    });
+  } catch (err) {
+    if (err.message === 'Admin already exists') {
+      return res.status(400).json({ message: 'Admin already exists' });
+    }
+    res.status(500).json({ message: 'Error creating admin', error: err.message });
+  }
+};
+
 exports.fetchUserById = async (req, res) => {
   const { id } = req.user;
   console.log(id)
--- model/User.js
+++ model/User.js
@@ -25,3 +25,5 @@ userSchema.set('toJSON', {
 });
 
 exports.User = mongoose.model('User', userSchema);
+
+
