8e6df2110dc01783e99b1b8ff0880d6084153b83
shivamp9381 <shivamp9381@gmail.com>
2025-02-14 01:10:37 +0530

    hua ni bhai

--- controller/Payment.js
+++ controller/Payment.js
@@ -1,20 +1,20 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const createPaymentIntent = async (req, res) => {
-  try {
-    const { totalAmount, orderId } = req.body;
+  try {
+    const { totalAmount, orderId } = req.body;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: totalAmount * 100,
-      currency: 'usd',
-      metadata: { orderId },
-    });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: totalAmount * 100,
+      currency: 'usd',
+      metadata: { orderId },
+    });
 
-    res.status(201).json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to create Payment Intent' });
-  }
+    res.status(201).json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to create Payment Intent' });
+  }
 };
-
+  
 module.exports = { createPaymentIntent };
